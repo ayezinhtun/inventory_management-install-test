33e46fb981517fbb2ac619e7f892e9a9183c26e2
Aye Zin Htun <ayezinhtun9@gmail.com>
2026-02-04 16:24:40 +0630

    update in notification

--- src/pages/Audit.jsx
+++ src/pages/Audit.jsx
@@ -160,13 +160,13 @@ export default function Audit() {
             >
               <span>Clear All</span>
             </div>
-            <div
+            {/* <div
               className="flex items-center border rounded-lg p-2 px-4 cursor-pointer text-white bg-[#26599F] hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-emerald-300 focus:ring-offset-2 transition"
               onClick={fetchAudits}
             >
               <Download className="w-5 h-5 mr-2" />
               <span>Export</span>
-            </div>
+            </div> */}
           </div>
         </div>
 
