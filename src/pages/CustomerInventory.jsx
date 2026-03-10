7227bbd6a49e9cf85e8ea555301596a7761b93a6
Aye Zin Htun <ayezinhtun9@gmail.com>
2026-03-10 11:02:05 +0630

    update

--- src/pages/CustomerInventory.jsx
+++ src/pages/CustomerInventory.jsx
@@ -36,6 +36,7 @@ export default function CustomerInventory() {
         (nameFilter === "" || c.customers?.contact_person === nameFilter)
     )
 
+    
     const currentInventorys = filteredInventorys;
 
     const fetchInventorys = async () => {
